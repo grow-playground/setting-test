0ba75807fb01defc81f56e8f6bea10d0569af20d
jgjgill <dbdltm22@naver.com>
2023-12-21 03:08:24 +0900

    feat: 예제 모달 구성

--- app/quizzes/[id]/quiz.tsx
+++ app/quizzes/[id]/quiz.tsx
@@ -1,15 +1,22 @@
 'use client';
 
+import Button from '@/components/common/buttons/button';
+import useTempModal from '@/components/common/modals/use-temp-modal';
 import { useGetQuiz } from '@/hooks/quiz';
 
 export function Quiz() {
   const { data: quiz } = useGetQuiz();
+  const { open } = useTempModal();
 
-  console.log(quiz);
+  const openModal = () => {
+    open();
+  };
+
   return (
     <div>
-      <div>나는 클라이언트 컴포넌트에서 불러온 값!</div>
+      {/* <div>나는 클라이언트 컴포넌트에서 불러온 값!</div> */}
       {JSON.stringify(quiz)}
+      <Button onClick={openModal}>모달 테스트</Button>
     </div>
   );
 }
