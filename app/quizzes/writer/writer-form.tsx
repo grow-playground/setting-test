966796368c18ad041446379d83371c68461ec872
bbearcookie <aodem@naver.com>
2023-12-25 02:28:57 +0900

    feat: 힌트 추가/삭제 기능 작성

--- app/quizzes/writer/writer-form.tsx
+++ app/quizzes/writer/writer-form.tsx
@@ -1,25 +1,105 @@
 'use client';
+
+import { useRef, useState } from 'react';
 import Button from '@/components/common/buttons/button';
+import { Button as ShadcnButton } from '@/components/ui/button';
 import MarkdownEditor from '@/components/common/markdown/markdown-editor';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Badge } from '@/components/ui/badge';
+import { Cross1Icon, PlusIcon } from '@radix-ui/react-icons';
 
 export default function WriterForm() {
+  const [hints, setHints] = useState<string[]>([]);
+  const hintRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const addHint = () => {
+    if (!hintRef.current || !hintRef.current.value) {
+      return;
+    }
+
+    const hint = hintRef.current.value;
+
+    if (hints.includes(hint)) {
+      return;
+    }
+
+    hintRef.current.value = '';
+    setHints((prev) => [...prev, hint]);
+  };
+
+  const removeHint = (idx: number) => {
+    setHints((prev) => prev.filter((_, i) => i !== idx));
+  };
+
   return (
-    <form>
-      <h2 className="text-lg font-bold">제목</h2>
-      <h2 className="text-lg font-bold">힌트</h2>
-      <MarkdownEditor>
-        <div className="mb-4 flex items-center justify-between">
-          <h2 className="grow text-lg font-bold">문제 내용</h2>
-          <MarkdownEditor.PreviewToggler>
-            {({ isPreview, toggle }) => (
-              <Button size="small" onClick={toggle}>
-                {isPreview ? '편집하기' : '미리보기'}
-              </Button>
-            )}
-          </MarkdownEditor.PreviewToggler>
+    <form onSubmit={onSubmit}>
+      <section className="mb-4">
+        <Label className="text-lg font-bold" htmlFor="title">
+          제목
+        </Label>
+        <Input id="title" />
+      </section>
+      <section className="mb-4">
+        <Label className="text-lg font-bold" htmlFor="hint">
+          힌트
+        </Label>
+        <div className="flex w-full space-x-2">
+          <Input
+            id="hint"
+            ref={hintRef}
+            autoComplete="off"
+            onKeyDown={(e) => e.key === 'Enter' && addHint()}
+          />
+          <ShadcnButton
+            type="button"
+            variant="secondary"
+            size="icon"
+            onClick={() => addHint()}
+          >
+            <PlusIcon />
+          </ShadcnButton>
         </div>
-        <MarkdownEditor.Content />
-      </MarkdownEditor>
+        <div className="mt-2 flex gap-2">
+          {hints.map((hint, idx) => (
+            <Badge
+              key={idx}
+              className="flex items-center gap-1"
+              variant="secondary"
+            >
+              {hint}
+              <Cross1Icon
+                className="cursor-pointer"
+                width={10}
+                height={10}
+                onClick={() => removeHint(idx)}
+              />
+            </Badge>
+          ))}
+        </div>
+      </section>
+      <section className="mb-4">
+        <MarkdownEditor>
+          <div className="mb-4 flex items-center justify-between">
+            <h2 className="grow text-lg font-bold">문제 내용</h2>
+            <MarkdownEditor.PreviewToggler>
+              {({ isPreview, toggle }) => (
+                <Button type="button" size="small" onClick={toggle}>
+                  {isPreview ? '편집하기' : '미리보기'}
+                </Button>
+              )}
+            </MarkdownEditor.PreviewToggler>
+          </div>
+          <MarkdownEditor.Content />
+        </MarkdownEditor>
+      </section>
+      <Button className="w-full" type="submit">
+        작성 완료
+      </Button>
     </form>
   );
 }
