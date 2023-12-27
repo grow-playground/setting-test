6766b28dfdac1ae547e4e3835d0a4acb8b06b956
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-12-28 04:37:18 +0900

    feat: 퀴즈 테이블 컴포넌트 적용

---
+++ app/user/[id]/_components/quiz-table.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { columns } from '@/components/quiz/table/columns';
+import DataTable from '@/components/quiz/table/data-table';
+import { useGetSubmittedQuiz } from '@/services/quiz/hooks';
+
+type QuizTableProps = {
+  userId: string;
+};
+
+export default function QuizTable({ userId }: QuizTableProps) {
+  const { data } = useGetSubmittedQuiz(userId);
+
+  return (
+    <div>{data && <DataTable columns={columns} data={data.quizzes} />}</div>
+  );
+}
