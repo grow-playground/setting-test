d1c2750f380d27e6159e188dd80795e67e375252
jgjgill <dbdltm22@naver.com>
2023-12-28 19:39:15 +0900

    fix: 배포 에러 작업 사이트맵 설정 변경

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,24 @@ import { MetadataRoute } from 'next';
 
 const PROD_URL = process.env.NEXT_PUBLIC_PROD_URL;
 
-export default function sitemap(): MetadataRoute.Sitemap {
+async function getQuizzes(): Promise<
+  Database['public']['Tables']['quizzes']['Row'][]
+> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/quizzes`);
+
+  const { quizzes } = await res.json();
+
+  return quizzes;
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const quizzes = await getQuizzes();
+
+  const quizSitemaps = quizzes.map((quiz) => ({
+    url: `${process.env.NEXT_PUBLIC_PROD_URL}/quizzes/${quiz.id}`,
+    lastModified: quiz.updated_at,
+  }));
+
   return [
     {
       url: `${PROD_URL}`,
@@ -10,5 +27,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
+    ...quizSitemaps,
   ];
 }
