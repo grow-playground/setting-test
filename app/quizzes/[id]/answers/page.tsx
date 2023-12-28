491411817234dc7dce8ac68aa325a27508eec458
bbearcookie <aodem@naver.com>
2023-12-28 18:30:20 +0900

    feat: 솔루션 페이지 컨텐츠 접근 제한 로직 작성

--- app/quizzes/[id]/answers/page.tsx
+++ app/quizzes/[id]/answers/page.tsx
@@ -11,6 +11,8 @@ import { cookies } from 'next/headers';
 import { createClient } from '@/utils/supabase/server';
 import { Separator } from '@/components/ui/separator';
 import commentOptions from '@/services/comment/options';
+import Link from 'next/link';
+import Button from '@/components/common/buttons/button';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const queryClient = new QueryClient();
@@ -23,21 +25,34 @@ export default async function Page({ params }: { params: { id: string } }) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const [submissions] = await Promise.all([
+  const [submission] = await Promise.all([
     queryClient.fetchQuery(
-      quizSubmissionOptions.submissions(session?.user.id ?? '')
+      quizSubmissionOptions.detailByQuiz(session?.user.id ?? '', quizId)
     ),
     queryClient.prefetchQuery(quizOptions.answers(quizId)),
     queryClient.prefetchQuery(commentOptions.quiz(quizId)),
   ]);
 
-  console.log(submissions);
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <QuizAnswer quizId={quizId} />
-      <Separator className="my-4" />
-      <Comments disable={!session} quizId={quizId} userId={session?.user.id} />
-    </HydrationBoundary>
-  );
+  if (submission?.success) {
+    return (
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <QuizAnswer quizId={quizId} />
+        <Separator className="my-4" />
+        <Comments
+          disable={!session}
+          quizId={quizId}
+          userId={session?.user.id}
+        />
+      </HydrationBoundary>
+    );
+  } else {
+    return (
+      <div className="flex flex-col items-center gap-6">
+        <h2 className="text-xl font-semibold">퀴즈를 먼저 맞춰주세요!</h2>
+        <Link replace href={`/quizzes/${quizId}`}>
+          <Button>퀴즈로 이동하기</Button>
+        </Link>
+      </div>
+    );
+  }
 }
--- app/quizzes/[id]/questions/page.tsx
+++ app/quizzes/[id]/questions/page.tsx
@@ -1,18 +1,15 @@
-'use client';
-
+import Link from 'next/link';
 import Button from '@/components/common/buttons/button';
-import { useParams, useRouter } from 'next/navigation';
 
-export default function Page() {
-  const router = useRouter();
-  const { id: quizId } = useParams();
+export default function Page({ params }: { params: { id: string } }) {
+  const quizId = Number(params.id) ?? 0;
 
   return (
     <div className="flex flex-col items-center gap-6">
-      <h2 className="text-xl font-semibold">준비중인 기능이에요.</h2>
-      <Button onClick={() => router.push(`/quizzes/${quizId}`)}>
-        문제로 이동하기
-      </Button>
+      <h2 className="text-xl font-semibold">준비중인 기능이에요!</h2>
+      <Link replace href={`/quizzes/${quizId}`}>
+        <Button>퀴즈로 이동하기</Button>
+      </Link>
     </div>
   );
 }
